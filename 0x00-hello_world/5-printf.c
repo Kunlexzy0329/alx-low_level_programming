a0b2077fece8a070fe25bf6468d412b7d29ac717
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-17 09:47:21 -0700

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- print string with printf
+ *
+ * Return:0
+ */
+int main()
+{
+	printf("with proper grammer, but the outcomeis a piece of art,\n");
+	return(0);
+}
