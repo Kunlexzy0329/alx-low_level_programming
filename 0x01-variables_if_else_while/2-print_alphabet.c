18192e2aa756729d1bb8caa5c79f6a270f8c22c6
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-18 06:00:56 -0700

    Printing alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  *main- start point
- *putchar function translate num to ascii 
+ * 
  *Return:0 if successful
  */
 
 int main(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 	{
 		putchar(i);
