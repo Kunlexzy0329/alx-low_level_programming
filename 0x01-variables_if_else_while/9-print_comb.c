4b5b9c6e6eb047c087c28b35c86a26cbdb1ced11
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-18 10:29:17 -0700

    Printing possible double numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 /**
- * main- start point 
+ * main-start point 
  *
  * Return:0 if successful
  */
 
-int main()
+int main(void)
 {
 	int a;
-	int b;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			putchar((a % 10) + '0');
-			putchar(',');
-			putchar((b % 10) + '0');
-		}
+		putchar((a % 10) + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
