a6050b02a534b1f7389b0c1d1652bc889a11b714
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-18 07:39:33 -0700

    Printing number using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-print numbers using putchar
+ *
+ * Return:0
+ */
+
+int main()
+{
+	int i=48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
