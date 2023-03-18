d631f458491bc05b99ef530cef71b8ce1fd265bd
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-18 08:22:03 -0700

    Smile in the mirror, printing reverse of alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- print alphabet in reverse order
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 122; i > 97; --i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
