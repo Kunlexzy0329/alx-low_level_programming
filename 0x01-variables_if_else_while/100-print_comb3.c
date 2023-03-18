e450ae14ac1fe32bb05135e498860e37b6ad5c63
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-18 11:46:29 -0700

    Combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main- start point
+ * putchar to print double number
+ * using nested loop to control the program
+ * Return:0 if true
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 8; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			if ((a % 10) == 8 && (b % 10) == 9)
+				{
+					continue;
+				}
+			putchar(44);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
