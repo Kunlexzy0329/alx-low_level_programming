740df6d5bf369b98159b4f8b10f682d43c97dfc9
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-23 15:00:28 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.c"
+/**
+ * _isalpha- function that print if parameter is lowercase or uppercase
+ * @c: parameter to be printed
+ * return 1 if the parameter is lowecase or uppercase
+ * and return 0 if otherwise
+ * Return:0 always
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
