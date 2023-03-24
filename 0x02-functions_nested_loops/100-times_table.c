ef895b436f56e5a9bd53602ab32b8d2ef7dc0e83
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-24 07:01:49 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_times_table - function that print times table
+ * @n : parameters that is multiplied with
+ *
+ * mult : takes the multiplication of both values
+ * Return : void
+ */
+
+void print_times_table(int n)
+{
+	int m;
+	int mult;
+
+	for (n = 0; n <= 12; n++)
+	{
+		for (m = 0; m <= 12; m++)
+		{
+			if (n < 0 || n > 15)
+			{
+				break;
+			}
+
+			mult = m * n;
+			_putchar(mult + '0');
+		}
+		_putchar('\n');
+	}
+}
