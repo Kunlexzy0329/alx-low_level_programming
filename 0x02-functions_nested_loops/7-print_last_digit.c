3329fde39ae6548fa1a728c24cc36ace1f961aff
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-23 23:50:24 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - fuction that print last digit
+ * @i: function parameter
+ * Return: k
+ */
+int print_last_digit(int i)
+{
+	int last;
+
+	last = i % 10;
+
+	if (i < 0)
+		last = -(last);
+	return (last);
+}
