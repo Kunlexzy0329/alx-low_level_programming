6a76b47e2c0f9701286bbbb4cbf734a9d152ed06
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-24 03:29:50 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - function to print numbers 0 through 98
+ *
+ * Return:0 always
+ *
+ */
+int main(void)
+{
+	int n;
+
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
