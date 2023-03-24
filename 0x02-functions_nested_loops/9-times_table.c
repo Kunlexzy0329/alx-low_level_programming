fcd13ce4c8b0a41debbaae920147d31ca4ce9fbc
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-24 02:18:24 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,34 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int x;
+	int y;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(i * j + '0');
-			_putchar(',');
-			_putchar('\n');
+			if (i * j > 9)
+			{
+				x = (i * j) % 10;
+				y = ((i * j) - x) / 10;
+
+				_putchar(',');
+				_putchar(' ');
+				_putchar(y + '0');
+				_putchar(x + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(i * j + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
