303623231ffe83d83da25219acc74d187018f627
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-24 08:57:40 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,10 +15,10 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; n++)
+		for (a = 0; a <= n; a++)
 		{
 			_putchar('0');
-			for (m = 0; m <= n; m++)
+			for (m = 1; m <= n; m++)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -40,9 +40,5 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
-	}
-	else if (n < 0 || n > 15)
-	{
-		_putchar('\n');
 	}
 }
