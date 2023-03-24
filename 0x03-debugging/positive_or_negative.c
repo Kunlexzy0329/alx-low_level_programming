e8f12a18164231c88f9d9aedd1d895d5ddb3f23b
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-24 16:48:32 -0700

    Positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - function to test for positive or negative values
+ * @i: parameter to be check
+ * Return: void
+ */
+
+void positive_or_negative(int i)
+{
+	if (i >= 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is negative\n", i);
+}
