0c2660697baba7a1467a70a709e96d46f44bcdfc
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-24 00:56:28 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - function that print the 9 times table with 0
+ * @i and @j: parameter to be multiplied
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j + '0');
+			_putchar(',');
+			_putchar('\n');
+		}
+	}
+}
