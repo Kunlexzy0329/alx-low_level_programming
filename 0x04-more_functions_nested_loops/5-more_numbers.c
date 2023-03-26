bdbcc0fa0b99c2104b8dfafa5e4220960710941c
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-03-26 13:55:31 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints 10 times the number
+ * from 0 to 14
+ * Return : void
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if(num > 9)
+			{
+				_putchar(num / 10 + '0');
+			}
+			_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
