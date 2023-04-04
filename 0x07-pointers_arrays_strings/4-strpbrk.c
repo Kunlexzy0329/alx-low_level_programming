dc871e3a478b8e00e3b7e335e2712e89a184ef84
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-04-03 18:33:22 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - function that search a string for a set of bytes
+ * @s: first string
+ * @accept: second string
+ * Return: a if first occurence is found and null if not found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *a = s;
+
+	while (*a != '\0')
+	{
+		char *t = accept;
+
+		while (*t != '\0')
+		{
+			if (*a == *t)
+			{
+				return (a);
+			}
+			t++;
+		}
+		a++;
+	}
+	return (NULL);
+}
