86c0bea03625aca8f7c920cf8f73c2187bb7f4b9
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-04-03 18:53:53 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - function that locate a subscript
+ * @haystack: pointer first string
+ * @needle: pointer to the second string
+ * Return: haystack if found or null if otherwise
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*b != '\0' && *a == *b)
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
