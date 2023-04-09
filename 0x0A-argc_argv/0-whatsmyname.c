7f8086406105fc67eebaaa511e965c350221dbdd
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-04-09 16:52:31 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - start point
+ * @argc: count the number of argument in tthe command line
+ * @argv: stored the argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
