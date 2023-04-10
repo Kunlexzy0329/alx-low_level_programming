2e825835a4ce7635043cae1da0980cb623830bab
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-04-09 18:20:14 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - start point
+ * @argc:count parameter
+ * @argv: unused in the program
+ * Return: 0 always
+ */
+
+int main(int argc, __attribute__((unused)) char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
