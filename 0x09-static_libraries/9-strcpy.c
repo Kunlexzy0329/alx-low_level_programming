fcf9c4ac13d1a1f2395ff81ffc68d478da9a79df
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-06-11 14:18:30 -0700

    Static_Libraries

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - function that copies the string pointed
+ * @dest: first variable
+ * @src: second variable
+ * Return: character copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
