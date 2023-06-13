c91d78bee7c93adb5ba163557e9a799605ed50b2
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-06-13 05:50:54 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  *Return: 0 always
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
