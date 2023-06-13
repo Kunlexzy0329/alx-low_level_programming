4eb8ddaae278dde1b7b607dc4d1a42d6ccd9c842
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-06-13 05:44:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - start point and it accepted two argument
+ *@argc: the argument counter
+ *@argv: the argument vector
+ *Return: 0 always
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
