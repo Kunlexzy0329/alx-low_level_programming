6d5bfaa28bbbbaa23e1445aed330b9f3ef194140
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-06-13 05:57:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - start of the program
+ *@argc: the argument count
+ *@argv: argument vector
+ *Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		printf("%d\n", argc);
+	return (0);
+}
