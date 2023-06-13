8c56458f8572c7070bec4fb6883a7b9535ba7877
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-06-13 16:28:19 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *main - start of the program
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	if (argc > 1)
+	{
+	for (i = 1; i < argc;  i++)
+	{
+		if (!(*argv[i] == '0') && !(*argv[i] <= '9'))
+		{
+			printf("Error\n");
+			return (0);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	}
+	return (0);
+}
