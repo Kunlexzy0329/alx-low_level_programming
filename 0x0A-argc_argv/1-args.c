211fa8b02e9c02bc0a518758ac61902b2e784cbc
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-07-07 16:01:55 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main- start of program
+ *@argc: the argument counter
+ *@argv: argument vector
+ *Return: 0 always
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
