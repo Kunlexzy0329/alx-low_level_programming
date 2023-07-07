e90429e9a432918f7dcde8029588633396a10c84
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-07-07 15:54:23 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main- start of the program
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: 0 always
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
