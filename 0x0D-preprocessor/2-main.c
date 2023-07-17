8c395f8c8f65de162c68baa474b72dcb1dff7e8c
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-07-17 12:13:38 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main- start program
+ *@void: the void parameter
+ *Return: 0 always
+ */
+
+int main(void)
+{
+	puts(__BASE_FILE__);
+	return (0);
+}
