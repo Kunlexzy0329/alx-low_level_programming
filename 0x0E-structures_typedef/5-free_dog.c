e72c0492e75c67fef0d6bcfab0e563cefe64ef61
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-07-18 09:42:43 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ *free_dog - function to free dog
+ *@d: the pointer to the dog document created
+ *Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
