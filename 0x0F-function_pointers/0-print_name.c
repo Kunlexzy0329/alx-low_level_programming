cb1e413d5b3354575ac75a60ef09b649a2d68c17
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-07-19 12:10:42 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - function that print name of a string
+ *@name: name of the string
+ *@f: function pointer
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (*name != '\0')
+		f(name);
+}
