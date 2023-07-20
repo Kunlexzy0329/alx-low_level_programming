84466b93044f9e5166058a9dab41590a6e836f16
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-07-20 16:59:46 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,7 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(x, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(x, const unsigned int);
+		sum += va_arg(x, int);
 	}
 	va_end(x);
 	return (sum);
