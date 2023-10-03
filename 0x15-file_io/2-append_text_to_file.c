912a04bea384411e285ae2b9c996ff3f7e3d238b
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-10-02 23:08:09 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, open_result, write_result;
+	int open_result, write_result;
 	int text_length = 0;
 
 	if (filename == NULL)
@@ -21,11 +21,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[text_length])
 			text_length++;
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	write_result = write(fd, text_content, text_length);
-	if (fd == -1 || write_result == -1)
+	open_result = open(filename, O_WRONLY | O_APPEND);
+	write_result = write(open_result, text_content, text_length);
+	if (open_result == -1 || write_result == -1)
 		return (-1);
-	close(fd);
+	close(open_result);
 	return (1);
 }
 
