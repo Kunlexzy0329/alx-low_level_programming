4297ec596b1a3bb821efc19e8f13b9f3b1c8178e
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2023-10-02 23:02:35 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, open_res, write_res;
+	int open_res, write_res;
 	int text_len = 0;
 
 	if (filename == NULL)
@@ -20,10 +20,10 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[text_len])
 			text_len++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write_res = write(fd, text_content, text_len);
-	if (fd == -1 || write_res == -1)
+	open_res = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_res = write(open_res, text_content, text_len);
+	if (open_res == -1 || write_res == -1)
 		return (-1);
-	close(fd);
+	close(open_res);
 	return (1);
 }
